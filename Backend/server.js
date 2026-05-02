dafc8b4145a7682f286fa6b4d2d93154ff066d52
Pranshu <ypranshu222@gmail.com>
2026-05-02 22:50:43 +0530

    fix deployment issue

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import geminiResponse from './gemini.js';
 
 const app = express()
 app.use(cors({
-    origin:[process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:["http://localhost:5173",
     "https://ai-voice-assistant-frontend-y1n9.onrender.com"
     ],
     credentials:true,
